a005bb7b03b97e3d418218dfe5b4e08e854d0258
Ryan Fox <71774297+rjfox1992@users.noreply.github.com>
2021-05-17 15:51:35 -0400

    node CLI app

---
+++ node-app/index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
+//Promise Method 2
+// const lstat = util.promisify(fs.lstat);
+
+// Promise Method 3
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const statPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDir, filename));
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(chalk.red(filenames[index]));
+    } else {
+      console.log(chalk.bold.green(filenames[index]));
+    }
+  }
+
+  // // Array Method
+  // const allStats = Array(filenames.length).fill(null);
+
+  // for (let filename of filenames) {
+  //   const index = filenames.indexOf(filename);
+
+  //   fs.lstat(filename, (err, stats) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     allStats[index] = stats;
+
+  //     const ready = allStats.every((stats) => {
+  //       return stats;
+  //     });
+
+  //     if (ready) {
+  //       allStats.forEach((stats, index) => {
+  //         console.log(filenames[index], stats.isFile());
+  //       });
+  //     }
+  //   });
+  // }
+});
+
+// Promise Method 1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
